3002adcae0bd63dd0fb6a3736209c27c1218e7ab
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-01-12 19:19:54 -0500

    Init people [ ]

---
+++ main.js
@@ -0,0 +1,21 @@
+//for/of iteration 
+const people = [
+    {
+        name: "Miss Daisy",
+        family: {
+            mother: "Polly Daisy",
+            father: "Stephen Daisy",
+            sister: "Lily Daisy",
+        },
+        age: 24,
+    },
+    {
+        name: "Tom Jones",
+        family: {
+            mother: "Norah Jones",
+            father: "Richard Jones",
+            brother: "Howie Jones",
+        },
+        age: 32,
+    },
+];
