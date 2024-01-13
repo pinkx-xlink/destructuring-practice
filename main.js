5073c8ca4a1ef021f89b0050c6c1dd95542d8486
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-01-12 19:36:03 -0500

    Run ESlint

--- main.js
+++ main.js
@@ -1,30 +1,30 @@
-//for/of iteration 
+// for/of iteration 
 const people = [
-    {
-        name: "Miss Daisy",
-        family: {
-            mother: "Polly Daisy",
-            father: "Stephen Daisy",
-            sister: "Lily Daisy",
-        },
-        age: 24,
+  {
+    name: 'Miss Daisy',
+    family: {
+      mother: 'Polly Daisy',
+      father: 'Stephen Daisy',
+      sister: 'Lily Daisy',
     },
-    {
-        name: "Tom Jones",
-        family: {
-            mother: "Norah Jones",
-            father: "Richard Jones",
-            brother: "Howie Jones",
-        },
-        age: 32,
+    age: 24,
+  },
+  {
+    name: 'Tom Jones',
+    family: {
+      mother: 'Norah Jones',
+      father: 'Richard Jones',
+      brother: 'Howie Jones',
     },
+    age: 32,
+  },
 ];
 
 for (const {
-    name: n,
-    family: { father: f },
+  name: n,
+  family: { father: f },
 } of people) {
-    console.log(`Name: ${n}, Father: ${f}`);
+  console.log(`Name: ${n}, Father: ${f}`);
 }
 // Name: Miss Daisy, Father: Stephen Daisy
 // Name: Tom Jones, Father: Richard Jones
@@ -34,6 +34,6 @@ for (const {
 // Destructuring can be used with property names 
 // that are not valid JavaScript identifiers 
 // by providing an alternative identifier that is valid.
-const foo = { "fizz-buzz": true }; //console.log = {fizz-buzz: true}
-const { "fizz-buzz": fizzBuzz } = foo; //console.log(foo) = {fizz-buzz: true}
-console.log(fizzBuzz); //true
+const foo = { 'fizz-buzz': true }; // console.log = {fizz-buzz: true}
+const { 'fizz-buzz': fizzBuzz } = foo; // console.log(foo) = {fizz-buzz: true}
+console.log(fizzBuzz); // true
